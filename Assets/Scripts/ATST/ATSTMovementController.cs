d9f8badd3023bdb29c9a20eb1851eb544dec446d
Artem <AsharnRus@mail.ru>
2018-12-01 11:53:18 +0300

    All animation update

---
+++ Assets/Scripts/ATST/ATSTMovementController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ATSTMovementController : MonoBehaviour {
+
+	public Animator Animator;
+
+	public Rigidbody Rigidbody;
+	public CapsuleCollider Collider;
+
+	// Use this for initialization
+	private void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		
+	}
+
+	private void FixedUpdate()
+	{
+		
+	}
+}
