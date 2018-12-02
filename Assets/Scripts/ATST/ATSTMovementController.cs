b30d41e24f212bd24f9b841e04036908131d3eb7
Artem <AsharnRus@mail.ru>
2018-12-02 22:54:55 +0300

    Update animations, settings up blend trees

--- Assets/Scripts/ATST/ATSTMovementController.cs
+++ Assets/Scripts/ATST/ATSTMovementController.cs
@@ -6,21 +6,37 @@ public class ATSTMovementController : MonoBehaviour {
 
 	public Animator Animator;
 
+	public Transform Transform;
 	public Rigidbody Rigidbody;
 	public CapsuleCollider Collider;
 
+	public JoystickAxes Joystick;
+
+	public float MovementSpeed = 30.0f;
+
 	// Use this for initialization
-	private void Start () {
+	private void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
-	private void Update () {
+	private void Update ()
+	{
+		Joystick.Horizontal = Input.GetAxis("Horizontal");
+		Joystick.Vertical = Input.GetAxis("Vertical");
+		Joystick.Round = Input.GetAxisRaw("Round");
+
+		Debug.Log("Horizontal = " + Joystick.Horizontal);
+		Debug.Log("Vertical = " + Joystick.Vertical);
+		Debug.Log("Round = " + Joystick.Round);
+		//Debug.Log(Input.GetJoystickNames().);
+
 		
 	}
 
 	private void FixedUpdate()
 	{
-		
+		Rigidbody.velocity = new Vector3(-Joystick.Vertical, 0, Joystick.Horizontal) * Time.deltaTime * MovementSpeed;
 	}
 }
---
+++ Assets/Scripts/ATST/JoystickAxes.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct JoystickAxes {
+
+	public float Vertical { get; set; }
+	public float Horizontal { get; set; }
+	public float Round { get; set; }
+}
